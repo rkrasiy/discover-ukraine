55f81f1d460e5f9ba143cea49534ff33a049f90b
Ruslan <krasiyruslan@gmail.com>
2022-11-02 23:53:52 +0100

    fix footer, header

--- src/pages/loyout.js
+++ src/pages/loyout.js
@@ -6,13 +6,14 @@ const Layout = () => {
 
   return (
     <div className="flex flex-col h-screen">
-        <header className="h-[50px]">
-            <SimpleTitle className="text-white ml-4 mt-4 uppercase font-extrabold" mini>Toukraine <img src="./logo-ukraine.png" className="w-10 inline" alt="logotipo"/></SimpleTitle>
+        <header className="h-[50px] fixed left-0 top-0 w-screen">
+            <SimpleTitle className="text-white ml-4 mt-4 uppercase font-extrabold" mini>Toukaine<img src="./logo-ukraine.png" className="w-10 inline" alt="Ukraine logo"/></SimpleTitle>
         </header>
         <main className="flex-grow flex items-center">
            <Outlet />
         </main>
-        <footer className="h-[60px] border-sky-800 border-t-4">
+        <footer className="border-sky-800 border-t-4 fixed left-0 bottom-0 w-screen">
+            <img src="./icons/ukraine-pattern.png" alt="ukraine pattern" className="w-full" />
         </footer>
     </div>
   )
