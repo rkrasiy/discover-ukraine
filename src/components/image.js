71a9ed532fdf17075821f942eead3e907a8fedb6
Ruslan <krasiyruslan@gmail.com>
2022-11-08 00:41:41 +0100

    fix select none

--- src/components/image.js
+++ src/components/image.js
@@ -1,7 +1,7 @@
 export default function Image(props){
     const {mini, small, large, huge, src, className, alt, object = "object-cover" } = props;
     let size = "w-[197px] h-[157px]";
-    let classes = "rounded-3xl";
+    let classes = "rounded-3xl md:select-all select-none";
 
     if(mini) size = "w-[107px] h-[118px]";
     if(small) size = "w-[110px] h-[125px]";
